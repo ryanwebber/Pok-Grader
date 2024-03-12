0ee1ef7c55c14577379856cb9798c668388b1941
Ryan Webber <wbbr.ryan@gmail.com>
2024-03-12 15:03:23 -0700

    Hacky hacky lazy rendering

--- src/index.ts
+++ src/index.ts
@@ -83,61 +83,83 @@ function imageForPokemon(name: string): string {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Insert the standard Pokedex into the DOM
-    const container = document.getElementById('list');
-    const dataList = document.getElementById('pokemon-data-list');
 
-    for (const key of Object.keys(standardPokedex)) {
+    // WARNING: Worlds worst lazy loading.
+    //
+    // This is a CPU bound task, so we can break it up across the
+    // event loop to let the browser do rendering, layout, and process
+    // other events.
 
-        // Construct the data-list item
-        let listItem = document.createElement('option') as HTMLOptionElement;
-        listItem.value = standardPokedex[key].name;
-        listItem.textContent = `#${standardPokedex[key].num}`;
-        dataList.appendChild(listItem);
+    let keys = Object.keys(standardPokedex);
+    let i = 0;
+    let handle = setInterval(() => {
+        console.log("Iteration", i);
+        let cardsFrag = document.createDocumentFragment();
+        let autocompleteFrag = document.createDocumentFragment();
 
-        // Construct the card
+        for (let j = 0; j < 20; j++) {
+            if (i >= keys.length) {
+                clearInterval(handle);
+                break;
+            }
 
-        let pokemon = standardPokedex[key];
-        let domNode = (document.getElementById("template-pokemon-item") as HTMLTemplateElement).content.cloneNode(true) as HTMLElement;
+            let key = keys[i];
 
-        let nameNode = domNode.querySelector("[data-tag-name]") as HTMLHeadingElement;
-        nameNode.innerText = pokemon.name;
+            // Construct the data-list item
+            let listItem = document.createElement('option') as HTMLOptionElement;
+            listItem.value = standardPokedex[key].name;
+            listItem.textContent = `#${standardPokedex[key].num}`;
+            autocompleteFrag.appendChild(listItem);
 
-        let noNode = domNode.querySelector("[data-tag-no]") as HTMLAnchorElement;
-        noNode.innerText = '#' + `${pokemon.num}`.padStart(3, '0');
-        noNode.href = `https://www.serebii.net/pokedex-swsh/${key}/`
+            // Construct the card
 
-        let imgNode = domNode.querySelector("[data-tag-image]") as HTMLImageElement;
-        let imgSource = imageForPokemon(key);
-        imgNode.src = imgSource;
-        imgNode.alt = pokemon.name;
+            let pokemon = standardPokedex[key];
+            let domNode = (document.getElementById("template-pokemon-item") as HTMLTemplateElement).content.cloneNode(true) as HTMLElement;
 
-        let gradeLetter = toGradeLetter(gradeSpecies(pokemon.name.toLowerCase()));
+            let nameNode = domNode.querySelector("[data-tag-name]") as HTMLHeadingElement;
+            nameNode.innerText = pokemon.name;
 
-        let gradeNode = domNode.querySelector("[data-tag-grade]") as HTMLSpanElement;
-        gradeNode.innerText = gradeLetter;
+            let noNode = domNode.querySelector("[data-tag-no]") as HTMLAnchorElement;
+            noNode.innerText = '#' + `${pokemon.num}`.padStart(3, '0');
+            noNode.href = `https://www.serebii.net/pokedex-swsh/${key}/`
 
-        let gradeStamp = domNode.querySelector("[data-tag-stamp]") as HTMLDivElement;
-        gradeStamp.classList.add(stampColors[gradeLetter]);
+            let imgNode = domNode.querySelector("[data-tag-image]") as HTMLImageElement;
+            let imgSource = imageForPokemon(key);
+            imgNode.src = imgSource;
+            imgNode.alt = pokemon.name;
 
-        let cardNode = domNode.querySelector("[data-tag-card]") as HTMLDivElement;
-        cardNode.classList.add(cardColors[gradeLetter]);
-        cardNode.id = `${pokemon.name}`;
+            let gradeLetter = toGradeLetter(gradeSpecies(pokemon.name.toLowerCase()));
 
-        let typesNode = domNode.querySelector("[data-tag-types]") as HTMLParagraphElement;
-        typesNode.innerText = pokemon.types.join(' / ');
+            let gradeNode = domNode.querySelector("[data-tag-grade]") as HTMLSpanElement;
+            gradeNode.innerText = gradeLetter;
 
-        let evolutionNode = domNode.querySelector("[data-tag-evolution]") as HTMLDivElement;
-        if (!pokemon.evos || pokemon.evos.length == 0) {
-            evolutionNode.style.display = 'none';
-        } else {
-            let imageNode = evolutionNode.querySelector("[data-tag-evolution-img]") as HTMLImageElement;
-            let imgSource = imageForPokemon(pokemon.evos[0]);
-            imageNode.src = imgSource;
+            let gradeStamp = domNode.querySelector("[data-tag-stamp]") as HTMLDivElement;
+            gradeStamp.classList.add(stampColors[gradeLetter]);
+
+            let cardNode = domNode.querySelector("[data-tag-card]") as HTMLDivElement;
+            cardNode.classList.add(cardColors[gradeLetter]);
+            cardNode.id = `${pokemon.name}`;
+
+            let typesNode = domNode.querySelector("[data-tag-types]") as HTMLParagraphElement;
+            typesNode.innerText = pokemon.types.join(' / ');
+
+            let evolutionNode = domNode.querySelector("[data-tag-evolution]") as HTMLDivElement;
+            if (!pokemon.evos || pokemon.evos.length == 0) {
+                evolutionNode.style.display = 'none';
+            } else {
+                let imageNode = evolutionNode.querySelector("[data-tag-evolution-img]") as HTMLImageElement;
+                let imgSource = imageForPokemon(pokemon.evos[0]);
+                imageNode.src = imgSource;
+            }
+
+            cardsFrag.appendChild(domNode);
+
+            i += 1;
         }
 
-        container.appendChild(domNode);
-    }
+        document.getElementById('cards-list').appendChild(cardsFrag);
+        document.getElementById('autocomplete-list').appendChild(autocompleteFrag);
+    }, 1);
 
     let searchInput = document.getElementById('pokemon-search') as HTMLInputElement;
     searchInput.addEventListener('change', (event) => {
